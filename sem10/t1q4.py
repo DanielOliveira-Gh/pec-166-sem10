c58ae204e064e153c0d772758c1cf988b360846e
Daniel Pereira <catce.2025211mtds0066@aluno.ifpi.edu.br>
2025-11-01 13:34:53 -0300

    adicionando comentarios e quebra de linha na saida de dados.

--- sem10/t1q4.py
+++ sem10/t1q4.py
@@ -1,8 +1,11 @@
+# variável auxiliar
 sequencia = ''
+# estrutura de repetição com for 
 for i in range(10,1001,10):
+# verificação da condicional
     if i == 1000:
         sequencia += str(f'{i}.')
     else:
         sequencia += str(f'{i},{" "}')
-
-print(f'Sua sequência de 1000 números de 10 em 10. {sequencia.strip()}')
+# saida de dados
+print(f'Sua sequência de 1000 números de 10 em 10.\n{sequencia.strip()}')
