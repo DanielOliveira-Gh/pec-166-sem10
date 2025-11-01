6597db1454b080dcdddf31e4ac4d7e1b3838562c
Daniel Pereira <catce.2025211mtds0066@aluno.ifpi.edu.br>
2025-10-31 22:00:34 -0300

    adicionando as mensagens de input e da saida. de dados.

--- t1q2.py
+++ t1q2.py
@@ -3,13 +3,14 @@ pares = 0
 impares = 0
 
 # entrada de dados 100 números
+print('Digite 100 números.')
 for i in range(1,101):
-    numero = int(input())
+    numero = int(input(f'Digite {i}° número: '))
 # verificar se e par ou impar
     if numero % 2 == 0:
         pares += 1
     else:
         impares += 1
 # saida de dados
-print(pares)
-print(impares)
+print(f'Quantidade de numeros pares {pares}')
+print(f'Quantidade de numeros impares {impares}')
