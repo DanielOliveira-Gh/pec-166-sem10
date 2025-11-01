8babe64ad0805a4423f7bbf500f8471b19fcd66b
Daniel Pereira <catce.2025211mtds0066@aluno.ifpi.edu.br>
2025-11-01 14:36:28 -0300

    retirando a condicional composta

--- sem10/t2q1.py
+++ sem10/t2q1.py
@@ -2,10 +2,8 @@
 cancao = ''
 # estrutura de repetição com for
 for i in range(99,251):
-# verificação da condicional composta
-    if i % 2 != 0:
-        cancao += str(f'{i} bugs no software, pegue um deles e conserte...\n')
-    else:
-        cancao += str(f'{i} bugs no software, pegue um deles e conserte...\n')
+# adicionando a string na variável auxiliar
+    cancao += str(f'{i} bugs no software, pegue um deles e conserte...\n')
+    
 # saida de dados
-print(f'Cação do Programador {cancao.strip()}')
+print(f'Cação do Programador.\n{cancao.strip()}')
